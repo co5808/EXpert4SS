76e11936212048101621b6032213d77950058808
co5808 <38681320+co5808@users.noreply.github.com>
2018-11-21 01:19:37 +0900

    expert system의 메인

---
+++ main.py
@@ -0,0 +1,12 @@
+import parser
+
+def main():
+    #loading rule txt
+        # '#' : fact의 목록 나열
+        # '+' : Fact가 가질수 있는 값
+    text = parser.RuleRead('\\Case\\Case1.txt')
+    #goal setting
+    #
+
+if __name__ == '__main__':
+    main()
