2194a57a27f334447a66a5279397c48466c93159
co5808 <38681320+co5808@users.noreply.github.com>
2018-11-21 23:59:04 +0900

    main에 Parser 연동

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import parser
 
 def main():
+    lossList = {}   #case 별 처리를 위한 장치 목록
+    rules = []      #Rule에 대한 반환값
+    goal = []       # gola 지정
     #loading rule txt
-        # '#' : fact의 목록 나열
-        # '+' : Fact가 가질수 있는 값
-    text = parser.RuleRead('\\Case\\Case1.txt')
-    #goal setting
-    #
+        # '#' : lossList의 목록 나열
+        # '+' : 각 LossList가 가질수 있는 값
+        # if ~ then : Rule
+        # '/' : goal 지정
+
+    lostList, rules, goal = parser.RuleRead('\\Case\\Case1.txt')
+
 
 if __name__ == '__main__':
     main()
